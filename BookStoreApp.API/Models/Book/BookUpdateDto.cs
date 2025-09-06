d1f6d2479828f2b1a102a2966255537d4bb752db
Nuno Martins <nuno.martins@perigorddata.com>
2025-09-06 16:02:13 +0100

    Completed Book Api Setup

--- BookStoreApp.API/Models/Book/BookUpdateDto.cs
+++ BookStoreApp.API/Models/Book/BookUpdateDto.cs
@@ -2,27 +2,22 @@
 
 namespace BookStoreApp.API.Models.Book
 {
-    public class BookCreateDto
+    public class BookUpdateDto : BaseDto
     {
         [Required]
         [StringLength(50)]
         public string Title { get; set; }
-
         [Required]
         [Range(1800, int.MaxValue)]
         public int Year { get; set; }
-
         [Required]
         public string Isbn { get; set; } = null!;
-
         [Required]
         [StringLength(250, MinimumLength = 10)]
         public string Summary { get; set; }
-
         public string Image { get; set; }
-
         [Required]
-        [Range(0, int.MaxValue)]
+        [Range(1800, int.MaxValue)]
         public decimal Price { get; set; }
 
     }
